c173cf7c6879ae441b87be833b18fec3cea25714
manikanta-1322 <allatiapallimanikantareddy@gmail.com>
2024-08-26 17:43:26 +0530

    Pushing Roboshop Ansible

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr_block" {}
+variable "public_subnets" {}
+variable "web_subnets" {}
+variable "app_subnets" {}
+variable "db_subnets" {}
+variable "azs" {}
+variable "tags" {}
+variable "env" {}
+variable "default_vpc_cidr" {}
+variable "account_id" {}
+variable "default_vpc_id" {}
+variable "default_route_table_id" {}
